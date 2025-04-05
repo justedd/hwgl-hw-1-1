74eb21a2ca5bf4cab42371c4050ab6415b8f81a2
justed <dev.justed@gmail.com>
2025-04-06 03:31:55 +0500

    Extract to internal

--- cmd/words-count/main.go
+++ cmd/words-count/main.go
@@ -1,100 +1,16 @@
 package main
 
 import (
-	"bufio"
-	"errors"
+	"github.com/justedd/hwgl-hw-1-1/internal/counter"
 	"fmt"
-	"os"
-	"regexp"
-	"sort"
-	"strings"
 )
 
-var errFileOpen = errors.New("unable to open file")
-
-type CountedWord struct {
-	Word  string
-	Count uint
-}
-
-func countWords(filePath string) ([]*CountedWord, error) {
-	file, err := os.Open(filePath)
-
-	defer func() {
-		err := file.Close()
-
-		if err != nil {
-			// TODO: use slog
-			fmt.Printf("Error while closing file: %v", err)
-		}
-	}()
-
-	if err != nil {
-		return nil, fmt.Errorf("[countWords] %w: %v", errFileOpen, err)
-	}
-
-	reg, err := regexp.Compile("[^a-z]+")
-	if err != nil {
-		return nil, fmt.Errorf("[countWords] regexp compile error: %v", err)
-	}
-
-	wordMap := make(map[string]*CountedWord)
-
-	scanner := bufio.NewScanner(file)
-	scanner.Split(bufio.ScanWords)
-
-	for scanner.Scan() {
-		raw := scanner.Text()
-		word := reg.ReplaceAllString(strings.ToLower(raw), "")
-
-		// TODO: ignore empty words
-		cw, ok := wordMap[word]
-
-		if !ok { 
-			cw = &CountedWord{Word: word, Count: 0}
-			wordMap[word] = cw
-		}
-
-		cw.Count += 1
-	}
-
-	result := make([]*CountedWord, 0, len(wordMap))
-
-	for _, cw := range wordMap {
-		result = append(result, cw)
-	}
-
-	return result, nil
-}
-
-func calculateTop(n uint, words []*CountedWord) []*CountedWord {
-	sort.Slice(words, func(i, j int) bool {
-		return words[i].Count > words[j].Count
-	})
-
-	topN := n
-	length := uint(len(words))
-	if topN > length {
-		topN = length
-	}
-
-	result := make([]*CountedWord, topN)
-	copy(result, words)
-
-	return result
-}
-
 func main() {
-	words, err := countWords("text.txt")
-
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	top := calculateTop(5, words)
+	// TODO: check errors
+	top := counter.FileTop(5, "text.txt")
 
 	for i := range top {
-		line := fmt.Sprintf("%s: %d", words[i].Word, words[i].Count)
+		line := fmt.Sprintf("%s: %d", top[i].Word, top[i].Count)
 		fmt.Println(line)
 	}
 }
---
+++ internal/counter/counter.go
@@ -0,0 +1,95 @@
+package counter
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"os"
+	"regexp"
+	"sort"
+	"strings"
+)
+
+var errFileOpen = errors.New("unable to open file")
+
+type CountedWord struct {
+	Word  string
+	Count uint
+}
+
+func countWords(filePath string) ([]*CountedWord, error) {
+	file, err := os.Open(filePath)
+
+	defer func() {
+		err := file.Close()
+
+		if err != nil {
+			// TODO: use slog
+			fmt.Printf("Error while closing file: %v", err)
+		}
+	}()
+
+	if err != nil {
+		return nil, fmt.Errorf("[countWords] %w: %v", errFileOpen, err)
+	}
+
+	reg, err := regexp.Compile("[^a-z]+")
+	if err != nil {
+		return nil, fmt.Errorf("[countWords] regexp compile error: %v", err)
+	}
+
+	wordMap := make(map[string]*CountedWord)
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanWords)
+
+	for scanner.Scan() {
+		raw := scanner.Text()
+		word := reg.ReplaceAllString(strings.ToLower(raw), "")
+
+		// TODO: ignore empty words
+		cw, ok := wordMap[word]
+
+		if !ok { 
+			cw = &CountedWord{Word: word, Count: 0}
+			wordMap[word] = cw
+		}
+
+		cw.Count += 1
+	}
+
+	result := make([]*CountedWord, 0, len(wordMap))
+
+	for _, cw := range wordMap {
+		result = append(result, cw)
+	}
+
+	return result, nil
+}
+
+func calculateTop(n uint, words []*CountedWord) []*CountedWord {
+	sort.Slice(words, func(i, j int) bool {
+		return words[i].Count > words[j].Count
+	})
+
+	topN := n
+	length := uint(len(words))
+	if topN > length {
+		topN = length
+	}
+
+	result := make([]*CountedWord, topN)
+	copy(result, words)
+
+	return result
+}
+
+func FileTop(topN uint, filename string) []*CountedWord {
+	words, err := countWords("text.txt")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	return calculateTop(topN, words)
+}
